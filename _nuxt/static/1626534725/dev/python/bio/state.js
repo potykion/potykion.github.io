fbe6c4646a44afde5d1072735a939060399d798d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-17 15:12:26 +0000

    Deploy potykion/potykion.github.io to github.com/potykion/potykion.github.io.git:gh-pages

---
+++ _nuxt/static/1626534725/dev/python/bio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626534725",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdev\u002Fpython\u002Fbio",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"236eaa95"}}}}(null));
