dd358702660f41b291fcdff0ffc568a468fd7b2f
potykion <potykion@gmail.com>
2022-10-17 21:32:52 +0300

    Фильтрация по label

--- toys/keep-board/keep_board_back/cloud_function.py
+++ toys/keep-board/keep_board_back/cloud_function.py
@@ -1,16 +1,15 @@
 import json
 
-from dotenv import load_dotenv
-
+from keep_board_back.src.yc import Event, Resp
 from src.api_cli import KeepCli
 
 
-def handler(event, context):
+def handler(event: Event, context) -> Resp:
     """
     https://functions.yandexcloud.net/d4evmq3b5u0kmmehthvf
     """
-    load_dotenv()
-    daily = KeepCli().setup().notes(label='daily')
+    label = event['queryStringParameters'].get('label', 'daily')
+    daily = KeepCli().setup().notes(label=label)
     daily = sorted(daily, key=lambda note: note.created)
     return {
         'statusCode': 200,
