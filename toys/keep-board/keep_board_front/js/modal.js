394f810f23f4e5edda8c4105c539271c8424b9e2
potykion <potykion@gmail.com>
2022-11-02 18:04:18 +0300

    :lipstick: Прогресс сохранения заметки

--- toys/keep-board/keep_board_front/js/modal.js
+++ toys/keep-board/keep_board_front/js/modal.js
@@ -2,7 +2,7 @@ export function setupModal(modalSelector) {
     const closeModal = () => document.querySelector(modalSelector).classList.remove('is-active');
     const openModal = () => document.querySelector(modalSelector).classList.add('is-active');
 
-    document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot')
+    document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete')
         .forEach(($close) => $close.addEventListener('click', () => closeModal()));
     document.addEventListener('keydown', (event) => {
         if ((event || window.event).keyCode === 27) closeModal();
