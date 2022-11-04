655669a96b2b5568619b7e8109c55d9ddb0c6a41
potykion <potykion@gmail.com>
2022-11-04 18:25:43 +0300

    :technologist: map can take any as param

--- toys/potyk-fp/potyk_fp/http.py
+++ toys/potyk-fp/potyk_fp/http.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Callable, Tuple, Union
+from typing import Callable, Tuple, Union, Any
 
 __all__ = ['HttpRes', 'Json', 'StatusCode', 'Code', 'Response']
 
@@ -12,19 +12,24 @@ Response = Tuple[Json, StatusCode]
 class HttpRes:
     success: bool
     msg: str = ''
-    code: int = None
+    _code: int = None
+
+    @property
+    def code(self):
+        return self._code or (200 if self.success else 400)
 
     @classmethod
-    def ok(cls, msg='', code=200, **kwargs):
-        return HttpRes(success=True, msg=msg, code=code)
+    def ok(cls, msg='', **kwargs):
+        return HttpRes(success=True, msg=msg, **kwargs)
 
     @classmethod
-    def err(cls, msg='', code=400, **kwargs):
-        return HttpRes(success=False, msg=msg, code=code)
+    def err(cls, msg='', **kwargs):
+        return HttpRes(success=False, msg=msg, **kwargs)
 
-    def map(self, callable_: Callable[[], 'HttpRes']):
+    def map(self, callable_: Callable[[], Union['HttpRes', Any]]):
         if self.success:
-            return callable_()
+            res = callable_()
+            return res if isinstance(res, HttpRes) else HttpRes(success=bool(res))
         else:
             return self
 
@@ -41,3 +46,8 @@ def test_HttpRes():
     assert HttpRes.ok('sam').as_response == ({'success': True, 'msg': 'sam'}, 200)
     assert HttpRes.ok('sam').map(lambda: HttpRes.ok('sam2')).msg == 'sam2'
     assert HttpRes.err('sam').map(lambda: HttpRes.ok('sam2')).msg == 'sam'
+    assert HttpRes.ok('sam').map(lambda: 'sam2').msg == ''
+    assert HttpRes.ok('sam').map(lambda: 'sam2').code == 200
+    assert HttpRes.ok('sam').map(lambda: False).msg == ''
+    assert HttpRes.ok('sam').map(lambda: False).code == 400
+    assert HttpRes.ok('sam').then(lambda: HttpRes.ok('sam2')).msg == 'sam2'
