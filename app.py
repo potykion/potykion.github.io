2971e25774fc2ee6e9789503361a9a9b4de9a34b
potykion <potykion@gmail.com>
2024-02-03 15:24:00 +0300

    md

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import frontmatter
+import mistune
+from flask import Flask, render_template, render_template_string
 
 app = Flask(__name__, template_folder="articles")
 
@@ -9,5 +11,21 @@ def index():
 
 
 @app.route("/<article>")
-def article(article):
-    return render_template(article)
+def article(article: str):
+    if article.endswith('.md'):
+        raw_md = render_template(article)
+        md = frontmatter.loads(raw_md)
+
+        html = mistune.html(md.content)
+        ctx = {
+            "show_title": True,
+            **md.metadata
+        }
+        return render_template_string(
+            "{% extends 'templates/base.html' %}"
+            "{% block main %}" + html + '{% endblock %}',
+            **ctx
+        )
+    else:
+        # html
+        return render_template(article)
