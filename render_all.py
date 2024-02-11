c2b2a24b3474373e084a264b1628ff1290ba8308
potykion <potykion@gmail.com>
2024-02-11 12:41:45 +0300

    render

--- render_all.py
+++ render_all.py
@@ -5,6 +5,21 @@ from pathlib import Path
 from app import app
 
 
+what_to_render = [
+    ("/", "index.html"),
+    ("/notes", "notes/index.html"),
+    *[
+        (f"/notes/{note_key}", f"notes/{note_key}.html")
+        for note_key in [
+            os.path.basename(note).rsplit(".")[0]
+            for note in glob.glob(f"{app.template_folder}/notes/**/*.*")
+        ]
+    ],
+    ("/special", "special/index.html"),
+    ("/special/stats", "special/stats.html"),
+]
+
+
 def make_server():
     return app.test_client()
 
@@ -15,15 +30,10 @@ def clean_and_create_dir(dist):
     os.mkdir(dist)
 
 
-def list_articles():
-    return glob.glob(f"{app.template_folder}/*.*")
-
-
-def render_article(article, server):
-    path = article_path_to_filename(article)
-    resp = server.get(f"/{path}", follow_redirects=True)
+def render(route, server):
+    resp = server.get(route, follow_redirects=True)
     if resp.status_code != 200:
-        raise RuntimeError(f"Failed to render page: {article}")
+        raise RuntimeError(f"Failed to render page: {route}")
     return resp.text
 
 
@@ -39,7 +49,10 @@ def make_filename(article):
 
 
 def write_article(dist, filename):
-    with open(dist / filename, "w", encoding="utf-8") as f:
+    path = dist / filename
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, "w", encoding="utf-8") as f:
         f.write(rendered)
 
 
@@ -53,12 +66,11 @@ if __name__ == "__main__":
     dist = Path("docs")
     clean_and_create_dir(dist)
 
-    articles = list_articles()
-    for index, article in enumerate(articles):
-        rendered = render_article(article, server)
-        filename = make_filename(article)
-        write_article(dist, filename)
-        print(f"Rendering articles: Progress: {index + 1} / {len(articles)}")
+    for index, route_and_path in enumerate(what_to_render):
+        route, path = route_and_path
+        rendered = render(route, server)
+        write_article(dist, path)
+        print(f"Rendering articles: Progress: {index + 1} / {len(what_to_render)}")
 
     print("Coping static")
     copy_static(dist)
