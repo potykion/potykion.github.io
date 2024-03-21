0223106da178a9db8c9a90ce47bce99e3cb64913
potykion <potykion@gmail.com>
2024-03-21 18:26:36 +0300

    wishlist.html

--- scripts/remove_bg.py
+++ scripts/remove_bg.py
@@ -1,10 +1,12 @@
 from rembg import remove
 
-input_path = r'0_0_orig.webp'
-output_path = '../static/images/beer/st-bernardus-tripel.png'
+if __name__ == '__main__':
 
-with open(input_path, 'rb') as i:
-    with open(output_path, 'wb') as o:
-        input = i.read()
-        output = remove(input)
-        o.write(output)
+    input_path = r'600x800 (1).webp'
+    output_path = '../static/images/wishlist/pozis.png'
+
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            input = i.read()
+            output = remove(input)
+            o.write(output)
