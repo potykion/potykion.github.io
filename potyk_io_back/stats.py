6d0844b3d9caf80f5f589039ba29d45b3df4109c
potykion <potykion@gmail.com>
2024-03-25 11:44:09 +0300

    mu from db

--- potyk_io_back/stats.py
+++ potyk_io_back/stats.py
@@ -11,6 +11,7 @@ def make_stats_blueprint(sqlite_cur):
     stats_blueprint = Blueprint("stats_blueprint", __name__)
 
     movies_storage = SimpleStorage(sqlite_cur, "movies")
+    music_storage = SimpleStorage(sqlite_cur, "music_albums")
 
     @stats_blueprint.route("/special/stats")
     def get_special():
@@ -19,12 +20,19 @@ def make_stats_blueprint(sqlite_cur):
             order_by="datetime(watched_dt)",
         )
 
+        albums = music_storage.list_all(
+            where="datetime(listened_dt) and datetime(listened_dt) >= datetime('2024-01-01')",
+            order_by="datetime(listened_dt)",
+        )
+
+
         return render_template(
             f"special/stats.html",
             show_title=True,
             show_desc=True,
             books=Books,
             movies=movies,
+            albums=albums,
         )
 
     return stats_blueprint
