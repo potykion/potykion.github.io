3dfb5f5759e9fcaaca05d5d0137625adf76a2dec
potykion <potykion@gmail.com>
2024-03-30 12:16:20 +0300

    2024-03-30

--- potyk_io_back/feed.py
+++ potyk_io_back/feed.py
@@ -15,23 +15,35 @@ def make_feed_blueprint(sqlite_cur: sqlite3.Cursor):
     def index():
         return render_template(
             "index.html",
-            beer=BeerStorage(sqlite_cur).get_by_id(38),
+            beer=BeerStorage(sqlite_cur).get_by_id(39),
         )
 
+    @feed_blueprint.route("/feed/2024-03-30")
+    def feed_4():
+        return index()
 
     @feed_blueprint.route("/feed/2024-03-27")
-    def two_days_ago():
+    def feed_1():
         return render_template(
             "feed/2024-03-27.html",
             beer=BeerStorage(sqlite_cur).get_by_id(21),
-            feed_date='2024-03-27',
+            feed_date="2024-03-27",
         )
+
     @feed_blueprint.route("/feed/2024-03-28")
-    def yesterday():
+    def feed_2():
         return render_template(
             "feed/2024-03-28.html",
             beer=BeerStorage(sqlite_cur).get_by_id(21),
-            feed_date='2024-03-28',
+            feed_date="2024-03-28",
+        )
+
+    @feed_blueprint.route("/feed/2024-03-29")
+    def feed_3():
+        return render_template(
+            "feed/2024-03-29.html",
+            beer=BeerStorage(sqlite_cur).get_by_id(38),
+            feed_date="2024-03-29",
         )
 
     return feed_blueprint
