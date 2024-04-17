c4e25146be8b55955366916cc5d070ee4edc7d1a
potykion <potykion@gmail.com>
2024-04-17 10:14:15 +0300

    botbto

--- scripts/trading/bot.py
+++ scripts/trading/bot.py
@@ -9,7 +9,7 @@ from urllib.parse import urlencode
 import requests
 from dotenv import load_dotenv
 from tabulate import tabulate
-from tinkoff.invest import Client, InstrumentIdType, InstrumentType
+from tinkoff.invest import Client, InstrumentIdType
 from tinkoff.invest.utils import money_to_decimal
 from tradingview_ta import get_multiple_analysis, Interval
 
@@ -167,7 +167,7 @@ TICKERS = [
 ]
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(kw_only=True)
 class PortfolioItem:
     ticker: str = ""
     figi: str = ""
@@ -175,6 +175,7 @@ class PortfolioItem:
     current_price: decimal.Decimal = decimal.Decimal(0)
     recommendation_1h: str = ""
     recommendation_1d: str = ""
+    recommendation_1w: str = ""
     ask_amount: int = 0
 
     @property
@@ -237,35 +238,37 @@ def main(force=False):
             ask_amount = sum((order.quantity for order in order_book.asks))
 
             portfolio_item = PortfolioItem(
-                ticker,
-                pos.figi,
-                money_to_decimal(pos.average_position_price).quantize(
+                ticker=ticker,
+                figi=pos.figi,
+                avg_price=money_to_decimal(pos.average_position_price).quantize(
                     decimal.Decimal("0.01")
                 ),
-                money_to_decimal(pos.current_price).quantize(decimal.Decimal("0.01")),
+                current_price=money_to_decimal(pos.current_price).quantize(
+                    decimal.Decimal("0.01")
+                ),
                 ask_amount=ask_amount,
             )
             portfolio_items.append(portfolio_item)
 
         my_tickers_with_exchange = [pos.ticker_with_exchange for pos in portfolio_items]
-
-        day_analysis = get_multiple_analysis(
-            screener="russia",
-            interval=Interval.INTERVAL_1_DAY,
-            symbols=my_tickers_with_exchange,
-        )
-        hour_analysis = get_multiple_analysis(
-            screener="russia",
-            interval=Interval.INTERVAL_1_HOUR,
-            symbols=my_tickers_with_exchange,
-        )
-        for pos in portfolio_items:
-            pos.recommendation_1d = day_analysis[pos.ticker_with_exchange].summary[
-                "RECOMMENDATION"
-            ]
-            pos.recommendation_1h = hour_analysis[pos.ticker_with_exchange].summary[
-                "RECOMMENDATION"
-            ]
+        if my_tickers_with_exchange:
+            day_analysis = get_multiple_analysis(
+                screener="russia",
+                interval=Interval.INTERVAL_1_DAY,
+                symbols=my_tickers_with_exchange,
+            )
+            hour_analysis = get_multiple_analysis(
+                screener="russia",
+                interval=Interval.INTERVAL_1_HOUR,
+                symbols=my_tickers_with_exchange,
+            )
+            for pos in portfolio_items:
+                pos.recommendation_1d = day_analysis[pos.ticker_with_exchange].summary[
+                    "RECOMMENDATION"
+                ]
+                pos.recommendation_1h = hour_analysis[pos.ticker_with_exchange].summary[
+                    "RECOMMENDATION"
+                ]
 
         portfolio_items = sorted(
             portfolio_items, key=lambda item: item.profit, reverse=True
@@ -328,71 +331,13 @@ def main(force=False):
             print_to_stream(tabulate_pre(table))
             print_to_stream()
 
-        print_to_stream("<b>Ideas</b>", file=tg_message_stream)
-
-        tickers_with_exchange = list(
-            {f"MOEX:{symbol}" for symbol in TICKERS} - set(my_tickers_with_exchange)
+        make_ideas(
+            print_to_stream,
+            tg_message_stream,
+            my_tickers_with_exchange,
+            client,
+            tabulate_pre,
         )
-        day_analysis = get_multiple_analysis(
-            screener="russia",
-            interval=Interval.INTERVAL_1_DAY,
-            symbols=tickers_with_exchange,
-        )
-        hour_analysis = get_multiple_analysis(
-            screener="russia",
-            interval=Interval.INTERVAL_1_HOUR,
-            symbols=tickers_with_exchange,
-        )
-        tickers_with_recs = []
-        for ticker in tickers_with_exchange:
-            tickers_with_recs.append(
-                (
-                    ticker,
-                    hour_analysis[ticker].summary["RECOMMENDATION"],
-                    day_analysis[ticker].summary["RECOMMENDATION"],
-                )
-            )
-        tickers_with_recs = [
-            PortfolioItem(
-                ticker=ticker[5:],
-                recommendation_1d=day.replace("STRONG_BUY", "BUY_STR"),
-                recommendation_1h=hour.replace("STRONG_BUY", "BUY_STR"),
-            )
-            for ticker, day, hour in tickers_with_recs
-            if {day, hour} & {"STRONG_BUY"} and {day, hour} & {"BUY"}
-        ]
-
-        for pos in tickers_with_recs:
-            instrument = client.instruments.get_instrument_by(
-                id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
-                class_code="TQBR",
-                id=pos.ticker,
-            )
-            pos.figi = instrument.instrument.figi
-
-            order_book = client.market_data.get_order_book(figi=pos.figi, depth=50)
-            ask_amount = sum((order.quantity for order in order_book.asks))
-            pos.ask_amount = ask_amount
-
-        tickers_with_recs = sorted(
-            [
-                (
-                    pos.ticker,
-                    pos.recommendation_1d,
-                    pos.recommendation_1h,
-                    pos.ask_amount,
-                )
-                for pos in tickers_with_recs
-            ],
-            key=(key_func := lambda item: (item[1], item[2], item[3])),
-            reverse=True,
-        )
-        tickers_with_recs = [
-            ["", "rec_1d", "rec_1h", "asks"],
-            *tickers_with_recs,
-        ]
-
-        print_to_stream(tabulate_pre(tickers_with_recs))
 
         TOKEN = os.getenv("TG_BOT_TOKEN")
         chat_id = os.getenv("TG_BOT_CHAT_ID")
@@ -410,6 +355,91 @@ def main(force=False):
         print(resp.json())  # this sends the message
 
 
+def make_ideas(
+    print_to_stream,
+    tg_message_stream,
+    my_tickers_with_exchange,
+    client,
+    tabulate_pre,
+):
+    print_to_stream("<b>Ideas</b>", file=tg_message_stream)
+
+    tickers_with_exchange = list(
+        {f"MOEX:{symbol}" for symbol in TICKERS} - set(my_tickers_with_exchange)
+    )
+    week_analysis = get_multiple_analysis(
+        screener="russia",
+        interval=Interval.INTERVAL_1_WEEK,
+        symbols=tickers_with_exchange,
+    )
+    day_analysis = get_multiple_analysis(
+        screener="russia",
+        interval=Interval.INTERVAL_1_DAY,
+        symbols=tickers_with_exchange,
+    )
+    hour_analysis = get_multiple_analysis(
+        screener="russia",
+        interval=Interval.INTERVAL_1_HOUR,
+        symbols=tickers_with_exchange,
+    )
+    tickers_with_recs = []
+    for ticker in tickers_with_exchange:
+        day = day_analysis[ticker].summary["RECOMMENDATION"]
+        hour = hour_analysis[ticker].summary["RECOMMENDATION"]
+
+        if not week_analysis[ticker]:
+            continue
+
+        week = week_analysis[ticker].summary["RECOMMENDATION"]
+
+        if (
+            week == "STRONG_BUY"
+            and day == "STRONG_BUY"
+            and hour in ("BUY", "STRONG_BUY")
+        ):
+            tickers_with_recs.append(
+                PortfolioItem(
+                    ticker=ticker[5:],
+                    recommendation_1d=day.replace("STRONG_BUY", "BUY_STR"),
+                    recommendation_1h=hour.replace("STRONG_BUY", "BUY_STR"),
+                    recommendation_1w=week.replace("STRONG_BUY", "BUY_STR"),
+                )
+            )
+
+    for pos in tickers_with_recs:
+        instrument = client.instruments.get_instrument_by(
+            id_type=InstrumentIdType.INSTRUMENT_ID_TYPE_TICKER,
+            class_code="TQBR",
+            id=pos.ticker,
+        )
+        pos.figi = instrument.instrument.figi
+
+        order_book = client.market_data.get_order_book(figi=pos.figi, depth=50)
+        ask_amount = sum((order.quantity for order in order_book.asks))
+        pos.ask_amount = ask_amount
+
+    tickers_with_recs = sorted(
+        [
+            (
+                pos.ticker,
+                pos.recommendation_1w,
+                pos.recommendation_1d,
+                pos.recommendation_1h,
+                pos.ask_amount,
+            )
+            for pos in tickers_with_recs
+        ],
+        key=(key_func := lambda item: (item[1], item[2], item[3], item[4])),
+        reverse=True,
+    )
+    tickers_with_recs = [
+        ["", "rec_1w", "rec_1d", "rec_1h", "asks"],
+        *tickers_with_recs,
+    ]
+
+    print_to_stream(tabulate_pre(tickers_with_recs))
+
+
 def handler(event, context):
     main()
     return {
@@ -419,4 +449,4 @@ def handler(event, context):
 
 
 if __name__ == "__main__":
-    main()
+    main(force=True)
